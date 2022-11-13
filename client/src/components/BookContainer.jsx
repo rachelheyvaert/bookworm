f6ddf299054fa97be2b97334c2924c3f61b70b10
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-13 13:41:59 -0500

    Creates components imports and exports

---
+++ client/src/components/BookContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookContainer = () => {
+  return (
+    <div>BookContainer</div>
+  )
+}
+
+export default BookContainer
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,3 +1,12 @@
-
-
+import React from 'react'
 import styled from 'styled-components'
+
+const Home = () => {
+  return (
+    <div>
+        <Image />
+        </div>
+  )
+}
+
+export default Home;

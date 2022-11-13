f168b4fa23084b1cd06a8deffb910cd6c66c91d6
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-13 09:22:26 -0500

    creates book methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,13 @@
 class BooksController < ApplicationController
+
+    def index 
+        render json: Book.all, status: :ok
+    end
+
+    def show
+        book = Book.find(params[:id])
+        render json: book, include: :reviews, status: :ok
+    end 
+
+    
 end

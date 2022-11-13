67939a73be98529934a12bc766a3fdcabd74a7cc
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-13 09:18:40 -0500

    validates user's reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  validates :rating, numericality: {greater_than: 0}
+  validates :rating, numericality: {less_than: 6}
+  validates :review_body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     belongs_to :book_club
     has_many :books, through :reviews
     has_many :reviews
-
+    has_secure_password 
 end

abd96564496a64f701b256fcf1f2072349bb6a80
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-13 09:33:36 -0500

    Creates User controller CRUD

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+    def index 
+        render json: Review.all, status: :ok
+    end 
+
+    def show
+        review = Review.find(params[:id])
+        render json: review, include: :book, status: :ok
+    end 
+
+    def create
+        review = Review.create!(review_params)
+        render json: review, status: :created
+    end 
+
+    def update 
+        review = Review.find(params[:id])
+        review.update!(review_params)
+        render json: review, status: :accepted
+    end 
+
+    def destroy
+        review = Review.find(params[:id])
+        review.destroy
+        head :no_content 
+    end 
+
+    private
+    
+    def review_params
+        params.permit(:rating, :review_body)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
-end
+    before_action :redirect_if_not_logged_in
+    skip_before_action :redirect_if_not_logged_in, only: [:new, :create]
+    
+    def new
+        @user = User.new
+    end
+
+    def show 
+        @user = User.find(params[:id])
+    end 
+
+    def edit
+        @user = User.find(params[:id])
+        redirect_to @user if @user != current_user
+      end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+          render :new
+        end
+      end
+
+      def update
+        redirect_to @user if @user != current_user
+        @user = User.find(params[:id])
+        @user.username = user_params[:username]
+        @user.save  
+        redirect_to user_path
+      end
+    
+      private
+    
+      def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
+    
+    end

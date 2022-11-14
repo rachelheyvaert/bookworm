5551f2e87a4aed3676b13e26a4e9f0ec4418d819
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-14 07:49:10 -0500

    installs correct version of mui packages

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,27 +1,9 @@
-import * as React from 'react';
-import ProductCategories from './modules/views/ProductCategories';
-import ProductSmokingHero from './modules/views/ProductSmokingHero';
-import AppFooter from './modules/views/AppFooter';
-import ProductHero from './modules/views/ProductHero';
-import ProductValues from './modules/views/ProductValues';
-import ProductHowItWorks from './modules/views/ProductHowItWorks';
-import ProductCTA from './modules/views/ProductCTA';
-import AppAppBar from './modules/views/AppAppBar';
-import withRoot from './modules/withRoot';
+import React from 'react'
 
-function Home() {
+const Home = () => {
   return (
-    <React.Fragment>
-      <AppAppBar />
-      <ProductHero />
-      <ProductValues />
-      <ProductCategories />
-      <ProductHowItWorks />
-      <ProductCTA />
-      <ProductSmokingHero />
-      <AppFooter />
-    </React.Fragment>
-  );
+    <div>Home</div>
+  )
 }
 
-export default withRoot(Home);
+export default Home

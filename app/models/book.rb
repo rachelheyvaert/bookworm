9d2f342a4c4b04040bed4ac0295f421bdca73c2a
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-18 17:32:32 -0500

    Adds seed data Books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
     has_many :reviews, dependent: :destroy
     has_many :users, through: :reviews
-
+    validates :description, presence: true, length: {maximum: 500}
 end
